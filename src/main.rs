d984bb4bbbfb144bba01a540b6a5eee2813dded5
luca-jt <random.lulu@die-genossen.de>
2024-06-08 15:58:52 +0200

    first version

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
 use std::env;
 use std::fs;
-
-static ARRAY: Vec<u8> = vec![0; 30000];
+use std::io::{stdin, stdout, Read, Write};
 
 fn main() {
+    // set up state
+    let mut cell: Vec<u8> = vec![0; 30000];
+    let mut ptr: usize = 0;
+
     // read input
     let args: Vec<String> = env::args().collect();
-    if args.len() != 1 {
-        panic!("provide exactly one arg");
+    if args.len() != 2 {
+        panic!("Provide exactly one input file as an argument.");
     }
-
-    let file: Vec<char> = fs::read(&args[0])
+    let chars: Vec<char> = fs::read(&args[1])
         .unwrap()
         .iter()
         .map(|byte| *byte as char)
         .collect();
 
-    let pointer: usize = 0;
-
     // interpret file
+    let mut i: usize = 0;
+    while i < chars.len() {
+        match chars[i] {
+            '+' => {
+                cell[ptr] += 1;
+            }
+            '-' => {
+                cell[ptr] -= 1;
+            }
+            '>' => {
+                ptr += 1;
+            }
+            '<' => {
+                ptr -= 1;
+            }
+            '.' => {
+                stdout().write(&[cell[ptr]]).unwrap();
+            }
+            ',' => {
+                stdin().read_exact(&mut [cell[ptr]]).unwrap();
+            }
+            '[' => {
+                if cell[ptr] == 0 {
+                    while chars[i] != ']' {
+                        i += 1;
+                    }
+                }
+            }
+            ']' => {
+                if cell[ptr] != 0 {
+                    while chars[i] != '[' {
+                        i -= 1;
+                    }
+                }
+            }
+            _ => {}
+        }
+        i += 1;
+    }
+    stdout().flush().unwrap();
 }
