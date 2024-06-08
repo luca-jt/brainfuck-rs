9249e504a1ac974ddda3df3170eeef67284656b4
luca-jt <random.lulu@die-genossen.de>
2024-06-08 11:20:42 +0200

    a bit of structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+
+static ARRAY: Vec<u8> = vec![0; 30000];
+
 fn main() {
-    println!("Hello, world!");
+    // read input
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 1 {
+        panic!("provide exactly one arg");
+    }
+
+    let file: Vec<char> = fs::read(&args[0])
+        .unwrap()
+        .iter()
+        .map(|byte| *byte as char)
+        .collect();
+
+    let pointer: usize = 0;
+
+    // interpret file
 }
